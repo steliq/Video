b4b7d3079304f83f22521d98e85d670d4b925f73
steliq <steliq@github.com>
2019-07-13 13:10:19 +0300

    Am creat un nou branch

--- TestInregistrat/src/TestVideo.java
+++ TestInregistrat/src/TestVideo.java
@@ -3,6 +3,8 @@ public class TestVideo {
 
 	public static void main(String[] args) {
 		System.out.println("Aici este branch-ul master");
+		
+		System.out.println("Am creat un branch nou");
 
 	}
 
